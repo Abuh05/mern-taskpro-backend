9ba1c0f72a92796508fc6eaa0e8446da1a95108b
Your Name <abuhsolo05@gmail.com>
2023-02-22 12:48:28 +0100

    updated server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:3000/", "https://mern-task-apps.onrender.com."],
+    origin: ["http://localhost:3000/", "https://mern-taskpro-app.onrender.com."],
   })
 );
 
